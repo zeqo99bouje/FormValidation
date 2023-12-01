a2ce66547a6aa7dc2279ab293e0edf60c7276d63
2002092200144@ofppt-edu.ma <2002092200144@ofppt-edu.ma>
2023-12-01 17:52:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import FormValidation from './FormValidation/FormValidation'
+
+export default function App() {
+  return (
+    <div>
+      <FormValidation />
+    </div>
+  )
+}
